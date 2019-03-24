d729e8ad589fd930529a7ef8bdd9b8d40e3374b2
curranlipsett <lipsettcurran7@gmail.com>
2019-03-24 12:30:50 -0500

    Add initial components and ui setup

---
+++ to-do-app/src/todo.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class ToDo extends React.Component {
+    render() {
+        let name = 'Curran';
+        return (
+            <div>
+                <h1 class="hello">Hello {name}!</h1>
+            </div>
+        );
+    }
+}
+
+export default ToDo
